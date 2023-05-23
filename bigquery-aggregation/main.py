40cc78c198932817932580a698d87a8197e87d3d
samytessier <samy@tessier.st>
2023-05-23 14:39:21 +0200

    base img

--- bigquery-aggregation/main.py
+++ bigquery-aggregation/main.py
@@ -1,19 +1,22 @@
+import logging
+from flask import abort, jsonify
 import datetime
 from google.cloud import bigquery
 
-def aggregate_data(event, context):
+
+def aggregate_data(request, context=None):
+    if request.method != 'POST':
+        return abort(405)
+    
     client = bigquery.Client()
-    dataset_id = 'main'
-    table_id_latest = 'sensor_data_latest'
-    table_id_m = 'sensor_data_m'
     now = datetime.datetime.utcnow()
     minute_start = now.replace(second=0, microsecond=0)
     minute_end = minute_start + datetime.timedelta(minutes=1)
     delete_threshold = now - datetime.timedelta(minutes=90)
     
-        # Aggregate the data
+    # Aggregate the data
     aggregation_query = f"""
-        INSERT INTO `{dataset_id}.{table_id_m}` (device_id, sensor_type, value, minute)
+        INSERT INTO `main.sensor_data_m` (device_id, sensor_type, value, minute)
         SELECT
             device_id,
             sensor_type,
@@ -26,7 +29,7 @@ def aggregate_data(event, context):
                 value,
                 timestamp,
                 ROW_NUMBER() OVER (PARTITION BY device_id, sensor_type, TIMESTAMP_TRUNC(timestamp, MINUTE) ORDER BY timestamp DESC) AS rn
-            FROM `{dataset_id}.{table_id_latest}`
+            FROM `main.sensor_data_latest`
             WHERE
                 timestamp >= '{minute_start.isoformat("T")}Z' AND
                 timestamp < '{minute_end.isoformat("T")}Z'
@@ -34,7 +37,7 @@ def aggregate_data(event, context):
     """
 
     deletion_query = f"""
-        DELETE FROM `{dataset_id}.{table_id_latest}` t1
+        DELETE FROM `main.sensor_data_latest`
         WHERE timestamp < '{delete_threshold.isoformat("T")}Z' 
             
     """
@@ -59,11 +62,14 @@ def aggregate_data(event, context):
             }
         }
     else:
-        return {
-            'success': True,
-            'rows_affected': {
-                'aggregation': rows_affected_agg,
-                'deletion': rows_affected_del
-            }
+        response_data = {
+        'success': True,
+        'rows_affected': {
+            'aggregation': rows_affected_agg,
+            'deletion': rows_affected_del,
+            'minute_start': minute_start.isoformat("T")+'Z',
+            'minute_end': minute_end.isoformat("T")+'Z'
         }
-        
+        }
+        logging.warning(f"Response data: {response_data}")  
+        return jsonify(response_data), 200
