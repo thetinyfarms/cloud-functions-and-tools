4676ab0e98bb2137fde7225e7458955b050d0d25
samytessier <samy@tessier.st>
2023-06-30 11:33:19 +0200

    new image storage location

--- pubsub-to-image/main.py
+++ pubsub-to-image/main.py
@@ -3,42 +3,41 @@ import json
 from google.cloud import storage
 from PIL import Image
 import io
+from datetime import datetime
 
 storage_client = storage.Client()
 
-def save_image_data(data, context):
-    if 'data' in data:
-        message = base64.b64decode(data['data']).decode('utf-8')
-        message_json = json.loads(message)
+def save_image_data(event, context):
+    # Access the base64 data directly from the 'data' attribute in the Pub/Sub message
+    message = base64.b64decode(event['data']).decode('utf-8')
 
-        device_id = message_json['device_id']
-        sensor_type = message_json['sensor_type']
-        value = message_json['value']
-        timestamp = message_json['timestamp']
+    # The deviceId is retrieved from the 'attributes' in the Pub/Sub message
+    device_id = event['attributes']['deviceId']
 
-        if sensor_type != 'camera':
-            print('Not a camera image, ignoring')
-            return
-
-        bucket_name = f'{device_id}_images'
-        bucket = storage_client.lookup_bucket(bucket_name)
+    bucket_name = 'tinyfarms-images' # Define your base bucket name here
+    bucket = storage_client.lookup_bucket(bucket_name)
         
-        if bucket is None:
-            print(f'Bucket {bucket_name} does not exist, creating one')
-            bucket = storage_client.create_bucket(bucket_name)
-            print(f'Bucket {bucket_name} created')
+    if bucket is None:
+        print(f'Bucket {bucket_name} does not exist, creating one')
+        bucket = storage_client.create_bucket(bucket_name)
+        print(f'Bucket {bucket_name} created')
+
+    # Create a folder within the bucket for each device
+    folder_name = f'{bucket_name}/{device_id}'
             
-        # Decode the base64 image data
-        image_data = base64.b64decode(value)
+    # Decode the base64 image data
+    image_data = base64.b64decode(message)
 
-        # Use PIL to determine the format of the image
-        image = Image.open(io.BytesIO(image_data))
-        format = image.format.lower()
+    # Use PIL to determine the format of the image
+    image = Image.open(io.BytesIO(image_data))
+    format = image.format.lower()
 
-        # Create a blob with the name based on the timestamp and the format
-        blob = bucket.blob(str(timestamp) + '.' + format)
-        blob.upload_from_string(image_data)
+    # Generate the timestamp
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 
-        print(f'Successfully saved image in {bucket_name}/{timestamp}.{format}')
-    else:
-        print('No data in message')
+    # Create a blob with the name based on the timestamp and the format
+    # It's stored in the folder corresponding to the device_id
+    blob = bucket.blob(f'{folder_name}/{timestamp}.{format}')
+    blob.upload_from_string(image_data)
+
+    print(f'Successfully saved image in {folder_name}/{timestamp}.{format}')
