38bbe05b301c67a69dd1435e3802c985e4b2f140
IRLadderGoat <bumbi@live.dk>
2023-07-12 13:01:06 +0200

    varying aggregation

--- bigquery-aggregation/main.py
+++ bigquery-aggregation/main.py
@@ -10,54 +10,65 @@ SCHEMAS = ["latest","m", "15m", "90m", "8h"]
 def aggregate_data(request, context=None):
     #if request.method != 'POST':
     #    return abort(405)
-    duration = "15m"#request.values.get('duration')
-    if duration not in DURATIONS:
-        return abort(400)
+    duration = "1m" # = request.values.get('duration')
+    #if duration not in DURATIONS:
+    #    return abort(400)
     
     client = bigquery.Client()
     assert client.project == 'tinyfarms-website'
     now = datetime.datetime.utcnow()
     end = now.replace(second=0, microsecond=0)
+    interval = int(duration[:-1])
     match duration:
-        case "1m" | "15m":
-            start = end - datetime.timedelta(minutes=int(duration[:-1])) # gets either 1 or 15
-        case "90m":
-            start = end - datetime.timedelta(minutes=90)
+        case "1m" | "15m" | "90m":
+            start = end - datetime.timedelta(minutes=interval) # gets either 1 or 15
+            time_scale = "MINUTE"
+            truncate = "HOUR"
         case "8h":
             start = end - datetime.timedelta(hours=8)
+            time_scale = "HOUR"
+            truncate = "DAY"
         case _:
             raise ValueError(f"Invalid duration: {duration}")
         
     duration_before = SCHEMAS[DURATIONS.index(duration)]
-    time_col = "timestamp" if duration_before == "latest" else "minute"
-    time_scale = "MINUTE" if 'm' in duration else "HOUR"
-    truncate = "HOUR" if 'm' in duration else "DAY"
-    interval = int(duration[:-1])
-    now.replace(second=0, microsecond=0)
-    delete_threshold = now - datetime.timedelta(minutes=90)
+    value_col = "average" if duration_before != "latest" else "value"
+    time_col = "timestamp"
+
+    delete_threshold = end - datetime.timedelta(minutes=90)
     
     # Aggregate the data
     aggregation_query = f"""
-        INSERT INTO `main.sensor_data_{duration}` (device_id, sensor_type, value, minute)
-        SELECT
-            device_id,
-            sensor_type,
-            value,
-            TIMESTAMP_ADD(TIMESTAMP_TRUNC({time_col}, {truncate}), INTERVAL CAST(ROUND(EXTRACT({time_scale} FROM {time_col}) / {float(interval)}) * {interval} AS INTEGER) {time_scale}) as minute
+        INSERT INTO `main.sensor_data_{duration}` (device_id, sensor_type, average, minimum, maximum, timestamp)
+        SELECT *
         FROM (
             SELECT
                 device_id,
                 sensor_type,
-                value,
-                {time_col},
-                ROW_NUMBER() OVER (PARTITION BY device_id, sensor_type, 
-                TIMESTAMP_ADD(TIMESTAMP_TRUNC({time_col}, {truncate}), INTERVAL CAST(ROUND(EXTRACT({time_scale} FROM {time_col}) / {float(interval)}) * {interval} AS INTEGER) {time_scale})
-                ORDER BY {time_col} DESC) AS rn
+                round(avg({value_col}), 2) as average,
+                min({value_col}) as minimum,
+                max({value_col}) as maximum,
+                TIMESTAMP_ADD(
+                    TIMESTAMP_TRUNC({time_col}, {truncate}), 
+                    INTERVAL 
+                    CAST(
+                        ROUND(
+                            EXTRACT({time_scale} FROM {time_col}) / {float(interval)}) * {interval} AS INTEGER)
+                    {time_scale}) as timestamp
             FROM `main.sensor_data_{duration_before}`
             WHERE
                 {time_col} >= '{start.isoformat("T")}Z' AND
                 {time_col} < '{end.isoformat("T")}Z'
-        ) WHERE rn = 1
+            GROUP BY device_id, sensor_type, timestamp
+        ) agg
+        WHERE NOT EXISTS (
+            SELECT device_id, sensor_type, timestamp
+            FROM `main.sensor_data_{duration}`
+            WHERE
+                device_id = agg.device_id AND
+                sensor_type = agg.sensor_type AND
+                timestamp = agg.timestamp
+        )
     """
 
     deletion_query = f"""
@@ -74,7 +85,7 @@ def aggregate_data(request, context=None):
     assert type(rows_affected_agg) == int
 
     deletion_job = client.query(deletion_query)
-    if duration == "8h":
+    if DURATIONS.index(duration) == len(DURATIONS) - 1:
         # Run the deletion query
         deletion_result = deletion_job.result()
         assert deletion_job.state == 'DONE'
@@ -85,20 +96,21 @@ def aggregate_data(request, context=None):
             'success': False,
             'errors': {
                 'aggregation': aggregation_job.errors,
-                'deletion': deletion_job.errors
+                'deletion': deletion_job.errors,
+                'duration': duration,
             }
         }
     else:
         response_data = {
         'success': True,
         'rows_affected': {
-            'aggregation': rows_affected_agg,
-            'deletion':  deletion_job.num_dml_affected_rows or 0 ,
-            'start': start.isoformat("T")+'Z',
-            'end': end.isoformat("T")+'Z'
-        }
+                'aggregation': rows_affected_agg,
+                'deletion':  deletion_job.num_dml_affected_rows or 0 ,
+                'start': start.isoformat("T")+'Z',
+                'end': end.isoformat("T")+'Z'
+            },
         }
         logging.warning(f"Response data: {response_data}")  
-        return jsonify(response_data), 200
-
+        # return jsonify(response_data), 200
+    
 aggregate_data(None)
