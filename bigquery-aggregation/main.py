484d6db559366ec3c4798b32dccac3c0c56b021f
IRLadderGoat <bumbi@live.dk>
2023-07-13 12:22:05 +0200

    finished aggregation based on time

--- bigquery-aggregation/main.py
+++ bigquery-aggregation/main.py
@@ -3,9 +3,11 @@ from flask import abort, jsonify, request
 import datetime
 from google.cloud import bigquery
 
-DURATIONS = ["1m", "15m", "90m", "8h"]
+# The durations we aggregate data for
+DURATIONS = ["1m", "15m", "90m", "480m"]
 # Threshold is how many minutes we keep the data for
-THRESHOLD = [1.5*60, 60*24*7, 60*24*30, 60*24*365] 
+THRESHOLD = [1.5*60, 60*24*7, 60*24*35, 60*24*80] 
+# The schemas we aggregate data from
 SCHEMAS = ["latest","1m", "15m", "90m", "8h"]
 
 def aggregate_data(context=None):
@@ -19,31 +21,20 @@ def aggregate_data(context=None):
         
     if duration not in DURATIONS:
         return abort(400)
+    interval = int(duration[:-1])
     
     client = bigquery.Client()
     assert client.project == 'tinyfarms-website'
+    
     now = datetime.datetime.utcnow()
     end = now.replace(second=0, microsecond=0)
-    interval = int(duration[:-1])
-    match duration:
-        case "1m" | "15m" | "90m":
-            start = end - datetime.timedelta(minutes=interval) # gets either 1 or 15
-            time_scale = "MINUTE"
-            truncate = "HOUR"
-        case "8h":
-            start = end - datetime.timedelta(hours=interval)
-            time_scale = "HOUR"
-            truncate = "DAY"
-        case _:
-            raise ValueError(f"Invalid duration: {duration}")
+    start = end - datetime.timedelta(minutes=interval) # gets either 1 or 15
         
     duration_index = DURATIONS.index(duration)
     duration_before = SCHEMAS[duration_index]
+    duration = SCHEMAS[duration_index+1]
     value_col = "average" if duration_before != "latest" else "value"
-    time_col = "timestamp"
 
-    delete_threshold = end - datetime.timedelta(minutes=THRESHOLD[duration_index])
-    
     # Aggregate the data
     aggregation_query = f"""
         INSERT INTO `main.sensor_data_{duration}` (device_id, sensor_type, average, minimum, maximum, timestamp)
@@ -56,16 +47,22 @@ def aggregate_data(context=None):
                 min({value_col}) as minimum,
                 max({value_col}) as maximum,
                 TIMESTAMP_ADD(
-                    TIMESTAMP_TRUNC({time_col}, {truncate}), 
+                    TIMESTAMP_TRUNC(timestamp, DAY), 
                     INTERVAL 
                     CAST(
                         ROUND(
-                            EXTRACT({time_scale} FROM {time_col}) / {float(interval)}) * {interval} AS INTEGER)
-                    {time_scale}) as timestamp
+                            EXTRACT(HOUR FROM timestamp) * 60
+                            +
+                            EXTRACT(MINUTE FROM timestamp)
+                        ) / {float(interval)} 
+                        AS INTEGER
+                    ) * {interval}
+                    MINUTE
+                ) as timestamp
             FROM `main.sensor_data_{duration_before}`
             WHERE
-                {time_col} >= '{start.isoformat("T")}Z' AND
-                {time_col} < '{end.isoformat("T")}Z'
+                timestamp >= '{start.isoformat("T")}Z' AND
+                timestamp < '{end.isoformat("T")}Z'
             GROUP BY device_id, sensor_type, timestamp
         ) agg
         WHERE NOT EXISTS (
@@ -78,10 +75,10 @@ def aggregate_data(context=None):
         )
     """
 
+    delete_threshold = end - datetime.timedelta(minutes=THRESHOLD[duration_index])
     deletion_query = f"""
-        DELETE FROM `main.{duration_before}`
+        DELETE FROM `main.sensor_data_{duration_before}`
         WHERE timestamp < '{delete_threshold.isoformat("T")}Z' 
-            
     """
 
      # Run the aggregation query
@@ -106,18 +103,19 @@ def aggregate_data(context=None):
             'errors': {
                 'aggregation': aggregation_job.errors,
                 'deletion': deletion_job.errors,
-                'duration': duration,
-            }
+            },
+            'duration': duration,
         }
     else:
         response_data = {
-        'success': True,
-        'rows_affected': {
-                'aggregation': rows_affected_agg,
-                'deletion':  rows_affected_del,
-                'start': start.isoformat("T")+'Z',
-                'end': end.isoformat("T")+'Z'
-            },
+            'success': True,
+            'rows_affected': {
+                    'aggregation': rows_affected_agg,
+                    'deletion':  rows_affected_del,
+                    'start': start.isoformat("T")+'Z',
+                    'end': end.isoformat("T")+'Z'
+                },
+            'duration': duration,
         }
         logging.warning(f"Response data: {response_data}")  
         return jsonify(response_data), 200
